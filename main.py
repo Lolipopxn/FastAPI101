3dce5064be98683bcddfe08f92df750a05011edc
Lolipop <xlolipopxn@gmail.com>
2025-06-30 17:16:46 +0700

    Step 1 - Database ORM Config

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from typing import Union
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+DATABASE_URL = "sqlite:///./sql_app.db"
+
+# ตั้งค่าและเชื่อมต่อกับฐานข้อมูล SQLite
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
     price: float
-
-@app.get("/hello ")
-def hello_world():
-    return {"message": "Hello World 3"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -29,3 +36,4 @@ def edit_item(item_id: int, item: Item):
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
     return { "massage": f"Item { item_id} deleted"}
+
